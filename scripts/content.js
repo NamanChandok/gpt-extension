29311a60e62f27c2d5b07eff68c4ea62928302af
Naman Chandok <namanchandok1@gmail.com>
2025-07-15 09:28:29 +0530

    Add API key input and storage

--- scripts/content.js
+++ scripts/content.js
@@ -26,6 +26,14 @@ const SITE_CONFIGS = {
   },
 };
 
+let apiKey;
+
+chrome.storage.sync.get("apiKey", (result) => {
+  if (result.apiKey) {
+    apiKey = result.apiKey;
+  }
+});
+
 const getCurrentWebsite = () => {
   const host = window.location.hostname;
   if (host.includes("chatgpt.com")) return "chatgpt";
@@ -69,6 +77,10 @@ const createButton = () => {
 
   const formatButton = document.createElement("button");
   formatButton.id = "prompt-enhancer-btn";
+  if (!apiKey) {
+    formatButton.disabled = true;
+    formatButton.style.cssText += "opacity: 0.5;";
+  }
   // Use more efficient SVG setup
   formatButton.innerHTML =
     '<svg class="w-[18px] h-[18px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m4.988 19.012 5.41-5.41m2.366-6.424 4.058 4.058-2.03 5.41L5.3 20 4 18.701l3.355-9.494 5.41-2.029Zm4.626 4.625L12.197 6.61 14.807 4 20 9.194l-2.61 2.61Z"/></svg>';
@@ -172,7 +184,7 @@ const createButton = () => {
 
     try {
       const res = await fetch(
-        "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyBaIdrU-XwT0kpANWQUOkqaMNs_qtvuDTc",
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
--- scripts/popup.js
+++ scripts/popup.js
@@ -2,11 +2,15 @@ const modeDefault = document.getElementById("mode_default");
 const modeDevelopment = document.getElementById("mode_development");
 const modeAcademic = document.getElementById("mode_academic");
 const modeFun = document.getElementById("mode_fun");
+const apiInput = document.getElementById("api_key");
+
+apiInput.addEventListener("blur", async (e) => {
+  const apiKey = e.target.value;
+  await chrome.storage.sync.set({ apiKey });
+});
 
 modeDefault.addEventListener("click", () => {
-  console.log(
-    "Default mode selected. This mode is suitable for general use."
-  )
+  console.log("Default mode selected. This mode is suitable for general use.");
   chrome.storage.sync.set({ mode: "default" });
   modeDevelopment.checked = false;
   modeAcademic.checked = false;
@@ -15,8 +19,8 @@ modeDefault.addEventListener("click", () => {
 
 modeDevelopment.addEventListener("click", () => {
   console.log(
-    "Development mode selected. This mode is suitable for developers."
-  )
+    "Development mode selected. This mode is suitable for developers.",
+  );
   chrome.storage.sync.set({ mode: "development" });
   modeDefault.checked = false;
   modeAcademic.checked = false;
